3514488ac92e0df70ae058f87be06eeeb309f2ff
lawvs <18554747+lawvs@users.noreply.github.com>
2022-04-29 01:41:06 +0800

    feat: add Eileen Dark theme

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {
   CarolColor,
   DarkBackground,
   DianaColor,
+  EileenColor,
   OUTPUT_DIR,
 } from "./config";
 import { genTheme } from "./genTheme";
@@ -278,6 +279,66 @@ const dianaDark = async () => {
   });
 };
 
+const eileenDark = async () => {
+  const theme = {
+    label: "Eileen Dark",
+    type: "dark",
+  } as const;
+
+  /**
+   * - color1 determines boolean, identifier, keyword, storage, and cssClass
+   * - color2 determines string, stringEscape, and cssId
+   * - color3 determines function, class, classMember, type, and cssTag
+   * - color4 determines functionCall and number
+   */
+  const colorSet: IColorSet = {
+    type: theme.type,
+    base: {
+      background: EileenColor.裙子,
+      foreground: EileenColor.皮肤,
+      color1: EileenColor.手环,
+      color2: EileenColor.头发暗部,
+      color3: EileenColor.眼睛亮部,
+      color4: EileenColor.鞋子,
+    },
+    ui: {
+      cursor: EileenColor.应援色,
+      selection: toHexColorString(transparentize(0.5, EileenColor.应援色)),
+    },
+    overrides: {
+      "statusBar.background": EileenColor.鞋子,
+      "statusBar.debuggingBackground": EileenColor.吊坠,
+    },
+  };
+
+  // Override token colors
+  const tokenColors = [
+    {
+      name: "object key",
+      scope: "meta.object-literal.key",
+      settings: {
+        foreground: EileenColor.皮肤,
+      },
+    },
+    {
+      name: "variable property",
+      scope: "variable.other.property",
+      settings: {
+        foreground: EileenColor.手环,
+      },
+    },
+  ] as const;
+
+  return genTheme({
+    label: theme.label,
+    type: theme.type,
+    colorSet: {
+      ...colorSet,
+      tokenColors,
+    },
+  });
+};
+
 type ContributesTheme = {
   label: string;
   uiTheme: "vs-dark" | "vs";
@@ -294,10 +355,11 @@ const main = async () => {
   );
 
   const themes: ContributesTheme[] = await Promise.all([
-    carolDark(),
     avaDark(),
     bellaDark(),
+    carolDark(),
     dianaDark(),
+    eileenDark(),
   ]);
 
   packageJson.contributes.themes = themes;
